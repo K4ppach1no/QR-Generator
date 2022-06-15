73ae117e153494fb8e3f586514b895f4dfab90f8
Kappachino <tgheeringa@gmail.com>
2022-06-15 16:02:05 +0200

    add name to qr code

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var mysql = require('mysql');
 var fs = require('fs');
 var qr = require('qr-image');
+var canvasLib = require('canvas');
 
 // Create a database connection
 var connection = mysql.createConnection({
@@ -10,30 +11,80 @@ var connection = mysql.createConnection({
     database: 'woonzorg'
 });
 
-
 // get adres and id from database
-connection.query('SELECT woning.id, complex.adres FROM complex INNER JOIN woning ON complex.id = woning.complex_id', function (error, results, fields) {
+connection.query('SELECT woning.id, complex.adres, woning.huisnummer FROM complex INNER JOIN woning ON complex.id = woning.complex_id', async (error, results, fields) => {
+    // handle error
     if (error) throw error;
+
 
     //loop through results
     for (var i = 0; i < results.length; i++) {
-        //create qr code for id
-        var qr_svg = qr.image(results[i].id, { type: 'png' });
 
-        // remove illegal characters from adres for filename
-        var filename = results[i].adres.replace(/[^a-zA-Z0-9]/g, '');
+        // check if all our variables exist
+        if (results[i].id == null || results[i].adres == null || results[i].huisnummer == null) {
+            console.log("Error: one of the variables is null");
+        } else {
+            // remove illegal characters from adres for filename
+            var filename = results[i].adres.replace(/[^a-zA-Z0-9]/g, '');
 
-        //create qr directory
-        if (!fs.existsSync('qr')) {
-            fs.mkdirSync('qr');
+            //create qr directory
+            if (!fs.existsSync('qr')) {
+                fs.mkdirSync('qr');
+            }
+
+            //create directory for qr code if not exists
+            if (!fs.existsSync('./qr/' + filename)) {
+                fs.mkdirSync('./qr/' + filename);
+            }
+
+            // generate a qr code for each woning, with their respective id.
+            var qrcode = qr.image(results[i].id, { type: 'png', size: 20 });
+            let file = fs.createWriteStream('./qr/' + filename + '/' + results[i].huisnummer + '.png');
+            let stream = qrcode.pipe(file);
+
+            // wait for the stream to finish
+            await new Promise((resolve, reject) => {
+                stream.on('finish', resolve);
+                stream.on('error', reject);
+            });
+
+            var filepath = './qr/' + filename + '/' + results[i].huisnummer + '.png';
+            // get the image
+            var image = await canvasLib.loadImage(filepath);
+            // create a canvas
+            var canvas = canvasLib.createCanvas(image.width, image.height);
+            // create a context
+            var ctx = canvas.getContext('2d');
+            // draw the image
+            ctx.drawImage(image, 0, 0);
+            // scale the font size to fit the image based on the text length
+            ctx.font = image.width / results[i].adres.length + 'px Arial';
+            // put the directory name below the image in the canvas
+            ctx.fillText(filename + " " + results[i].huisnummer, 5, image.height - 5);
+
+            //create a promise to wait for the canvas to be saved
+            var promise = new Promise((resolve, reject) => {
+                canvas.toBuffer(function (err, buffer) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        // save the buffer
+                        fs.writeFile(filepath, buffer, function (err) {
+                            if (err) {
+                                console.log(err);
+                            } else {
+                                console.log("The file was saved!" + filepath);
+                                // resolve the promise
+                                resolve();
+                            }
+                        });
+                    }
+                });
+            }
+            );
+            // wait for the promise to finish
+            await promise;
         }
-
-        //create directory for qr code if not exists
-        if (!fs.existsSync('./qr/' + filename)) {
-            fs.mkdirSync('./qr/' + filename);
-        }
-
-        qr_svg.pipe(fs.createWriteStream('./qr/' + filename + '/' + results[i].id + '.png'));
     }
 
     //close connection
